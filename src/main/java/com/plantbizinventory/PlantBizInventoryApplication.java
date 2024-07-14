4a14eaea7c72d373a57a9dd3f300b65209ea9a26
ChetanSawant <chetansawant.dev@gmail.com>
2024-07-14 16:06:16 +0530

    Initial Commit For Inventory Module

---
+++ src/main/java/com/plantbizinventory/PlantBizInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.plantbizinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantBizInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantBizInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantbizinventory/PlantBizInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantbizinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantBizInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
